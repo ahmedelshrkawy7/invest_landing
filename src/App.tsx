48da4104dbf230f67f917a4b400a5771db2f355f
alshrkawy <ahmadelshrkawy232@gmail.com>
2025-01-11 17:36:14 +0200

    last

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,6 @@ import Roadmap from "./components/Roadmap";
 function App() {
   return (
     <>
-      <div id="tm-preloader">
-        <div className="tm-preloader-in">
-          <div className="bar bar-top"></div>
-          <div className="bar bar-right"></div>
-          <div className="bar bar-bottom"></div>
-          <div className="bar bar-left"></div>
-        </div>
-      </div>
       <Header />
       <NavigationBar />
       <HeroSection />
